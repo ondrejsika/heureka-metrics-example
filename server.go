e8ba297c9d48c1f6277a31f6cd680b87e2a53460
Ondrej Sika <ondrej@ondrejsika.com>
2022-02-28 10:33:03 +0100

    feat(server): Add metric example_requests_total

--- server.go
+++ server.go
@@ -6,13 +6,29 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
+var counter_requests = prometheus.NewCounter(
+	prometheus.CounterOpts{
+		Namespace: "example",
+		Name:      "requests_total",
+		Help:      "Total nuber of requests",
+	})
+
 func main() {
+	prometheus.MustRegister(counter_requests)
+
 	http.Handle("/metrics", promhttp.Handler())
 
+	http.HandleFunc("/favicon.ico", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(404)
+	})
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println(r.URL.Path)
+		counter_requests.Inc()
 		w.WriteHeader(200)
 		w.Write([]byte("OK"))
 	})
